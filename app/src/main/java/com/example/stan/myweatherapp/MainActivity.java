4cff5aa0de4d9ce57253f2cf1de82861eb46845f
Stan402 <konvin@yahoo.com>
2017-11-06 17:17:41 +0300

    Android: Weather app: added lesson4hw task1-2.

--- app/src/main/java/com/example/stan/myweatherapp/MainActivity.java
+++ app/src/main/java/com/example/stan/myweatherapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -12,7 +13,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "############";
+
     static final int REQUEST_CODE = 5;
+    private static final String PREVIOUS_CITY_INFO = "previous city info";
     private TextView textviewUnder;
     private Spinner spinnerForCity;
 
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textviewUnder = (TextView) findViewById(R.id.textview_under);
@@ -43,22 +48,24 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onPause() {
-        super.onPause();
+        Log.d(TAG, "onPause");
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putInt(SAVED_CITY, spinnerForCity.getSelectedItemPosition());
         editor.apply();
+        super.onPause();
     }
 
     @Override
     protected void onResume() {
+        Log.d(TAG, "onResume");
         super.onResume();
-            spinnerForCity.setSelection(sharedPreferences.getInt(SAVED_CITY, 0));
+        spinnerForCity.setSelection(sharedPreferences.getInt(SAVED_CITY, 0));
         //showForecastForCity();
     }
 
     private void showForecastForCity() {
         //textviewForecast.setText(WeatherSpec.getForecast(MainActivity.this, spinnerForCity.getSelectedItemPosition()));
-        Intent intent =new Intent(MainActivity.this, DisplayForecastActivity.class);
+        Intent intent = new Intent(MainActivity.this, DisplayForecastActivity.class);
         intent.putExtra(DisplayForecastActivity.CITY_TAG, spinnerForCity.getSelectedItemPosition());
         startActivityForResult(intent, REQUEST_CODE);
         //startActivity(intent);
@@ -67,10 +74,45 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE){
+        if (requestCode == REQUEST_CODE) {
             int cityNumber = data.getIntExtra(DisplayForecastActivity.RESULT_TAG, 0);
             textviewUnder.setText(String.format("The weather forecast shown for %s",
-                                WeatherSpec.getCityName(MainActivity.this, cityNumber)));
+                    WeatherSpec.getCityName(MainActivity.this, cityNumber)));
         }
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState:" + savedInstanceState);
+        if (savedInstanceState != null)
+            textviewUnder.setText(savedInstanceState.getCharSequence(PREVIOUS_CITY_INFO));
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState");
+        //Я не стал сохранять остальные настройки, потому что они и так восстанавливаются из sharedPreferance
+        //Единственное что не сохраняется при пересоздании это инфа о предыдущем просмотре - ее и сохраняем
+        outState.putCharSequence(PREVIOUS_CITY_INFO, textviewUnder.getText());
+        super.onSaveInstanceState(outState);
     }
 }
